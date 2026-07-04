ed63648ef68fa9397f58f75b85f0a4a1217bf109
Faizan-mk <apploginmk@gmail.com>
2026-07-04 12:47:13 +0500

    Add Dockerfile and .dockerignore; implement static file serving in index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,11 +4,14 @@ import user  from "./models/user_model.js"
 import { connectDB } from "./config/db.js"
 import { clerkMiddleware } from '@clerk/express'
 import cors from 'cors'
+import fs from 'fs'
+import path from 'path'
 
 
 const app=express()
 const port=process.env.port
 const frontendUrl=process.env.forntend_url
+const publicdir=path.join(process.cwd(),'public')
 app.use(express.json())
 app.use(cors({origin:forntend_url, credentials:true}))
 app.use(clerkMiddleware())
@@ -16,6 +19,14 @@ app.use(clerkMiddleware())
 app.get("/hello",(req,res)=>{
     res.status(200).json({message:"hello world"})
 })
+
+if(fs.existssync(publicdir)){
+   app.use(express.static(publicdir))
+
+   app.get("/{*any}",(req,res,next)=>{
+    res.sendFile(path.join(publicdir,'index.html'),(err) => next(err));
+   });
+}
 
 app.listen(port,()=>{
     connectDB()
